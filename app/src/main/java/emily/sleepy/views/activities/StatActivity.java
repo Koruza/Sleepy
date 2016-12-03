9accd2212d4d3d2406a147715af411d9412ed0f2
Ka Wo Fong <kwfong@umass.edu>
2016-12-02 22:14:49 -0500

    created statActivity and MainAct after adding other new files

---
+++ app/src/main/java/emily/sleepy/views/activities/StatActivity.java
@@ -0,0 +1,25 @@
+package emily.sleepy.views.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import emily.sleepy.R;
+
+public class StatActivity extends AppCompatActivity {
+
+    TextView phoneUsageTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat);
+
+        phoneUsageTimer =(TextView) findViewById(R.id.textViewPhoneUsageTimer);
+    }
+
+}
